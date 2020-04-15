a564162fc3a1bdfb44afa520c37b14880bed86c4
Wnao <43458392+Wnao@users.noreply.github.com>
2020-04-14 23:56:30 -0300

    password v2.4 Exer

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/DetalhesSenhaActivity.java
@@ -10,6 +10,10 @@ public class DetalhesSenhaActivity extends AppCompatActivity {
 
     private Senha senha;
     private TextView textViewSenha;
+    private TextView textViewSenha1;
+    private TextView textViewSenha2;
+    private TextView textViewSenha3;
+    private TextView textViewSenha4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,13 @@ public class DetalhesSenhaActivity extends AppCompatActivity {
         senha = (Senha)intent.getSerializableExtra("senha");
         textViewSenha = findViewById(R.id.textViewNomeValue);
         textViewSenha.setText(senha.getNome());
+        textViewSenha1 = findViewById(R.id.textViewNomeValue1);
+        textViewSenha1.setText(senha.getNome());
+        textViewSenha2 = findViewById(R.id.textViewNomeValue2);
+        textViewSenha2.setText(senha.getNome());
+        textViewSenha3 = findViewById(R.id.textViewNomeValue3);
+        textViewSenha3.setText(senha.getNome());
+        textViewSenha4 = findViewById(R.id.textViewNomeValue4);
+        textViewSenha4.setText(senha.getNome());
     }
 }
